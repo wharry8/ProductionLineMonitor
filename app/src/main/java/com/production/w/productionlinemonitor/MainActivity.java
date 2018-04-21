8629451dafa8238c68ca7ce51ddfa7a125b34193
TorikoC <breakthroughtgw@gmail.com>
2018-04-21 16:08:02 +0800

    finish main screen template.

--- app/src/main/java/com/production/w/productionlinemonitor/MainActivity.java
+++ app/src/main/java/com/production/w/productionlinemonitor/MainActivity.java
@@ -2,12 +2,17 @@ package com.production.w.productionlinemonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // hide action bar.
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
     }
 }
