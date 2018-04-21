3d0d3d2e63ebb8f0aecd2ae7a24e36952b07091b
TorikoC <breakthroughtgw@gmail.com>
2018-04-21 16:51:53 +0800

    add navigation drawer.

--- app/src/main/java/com/production/w/productionlinemonitor/MainActivity.java
+++ app/src/main/java/com/production/w/productionlinemonitor/MainActivity.java
@@ -1,18 +1,44 @@
 package com.production.w.productionlinemonitor;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // hide action bar.
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+        initNavigationDrawer();
+    }
+
+    public void initNavigationDrawer() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        return true;
+                    }
+                });
     }
 }
