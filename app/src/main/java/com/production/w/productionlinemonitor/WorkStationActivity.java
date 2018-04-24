9ff9ddfe021e4397dee97c84aa24d9a1dc2fbbf0
TorikoC <breakthroughtgw@gmail.com>
2018-04-24 20:27:35 +0800

    add navigation drawer to WorkStationListActivity and WorkStationActivity.

--- app/src/main/java/com/production/w/productionlinemonitor/WorkStationActivity.java
+++ app/src/main/java/com/production/w/productionlinemonitor/WorkStationActivity.java
@@ -1,11 +1,18 @@
 package com.production.w.productionlinemonitor;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class WorkStationActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    String TAG = "WorkStationActiviy";
     private TextView tv_name;
     private TextView tv_status;
     private TextView tv_up_time;
@@ -21,8 +28,45 @@ public class WorkStationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_station);
 
+        initNavigationDrawer();
         bind();
         updateView();
+    }
+    public void initNavigationDrawer () {
+
+        mDrawerLayout = findViewById(R.id.ws_drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.ws_nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+                        String selectedTitle = (String) menuItem.getTitle();
+                        Log.e(TAG, "onNavigationItemSelected: " + selectedTitle);
+                        Intent intent;
+
+                        if (selectedTitle == getString(R.string.main)) {
+                            intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else if (selectedTitle == getString(R.string.productionLine)) {
+                            intent = new Intent(getApplicationContext(), ProductionLineActivity.class);
+                            startActivity(intent);
+                        } else if (selectedTitle == getString(R.string.workers)) {
+                            intent = new Intent(getApplicationContext(), WorkStationListActivity.class);
+                            startActivity(intent);
+                        } else {
+
+                        }
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        return true;
+                    }
+                });
     }
 
     public void bind() {
--- app/src/main/java/com/production/w/productionlinemonitor/WorkStationListActivity.java
+++ app/src/main/java/com/production/w/productionlinemonitor/WorkStationListActivity.java
@@ -1,15 +1,24 @@
 package com.production.w.productionlinemonitor;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class WorkStationListActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private DrawerLayout mDrawerLayout;
+
+    private static final String TAG = "WorkStationListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,39 @@ public class WorkStationListActivity extends AppCompatActivity {
         init();
     }
     public void initNavigationDrawer () {
+
+        mDrawerLayout = findViewById(R.id.wsl_drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.wsl_nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+                        String selectedTitle = (String) menuItem.getTitle();
+                        Log.e(TAG, "onNavigationItemSelected: " + selectedTitle);
+                        Intent intent;
+
+                        if (selectedTitle == getString(R.string.main)) {
+                            intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else if (selectedTitle == getString(R.string.productionLine)) {
+                            intent = new Intent(getApplicationContext(), ProductionLineActivity.class);
+                            startActivity(intent);
+                        } else if (selectedTitle == getString(R.string.workers)) {
+
+                        } else {
+
+                        }
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        return true;
+                    }
+                });
 
     }
     public void init () {
