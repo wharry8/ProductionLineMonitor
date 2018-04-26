b92b754cf28a18e089a342b47a729f62ea205f8b
TorikoC <breakthroughtgw@gmail.com>
2018-04-26 12:59:17 +0800

    add register mapping.

---
+++ app/src/main/java/com/production/w/productionlinemonitor/Helper/Coil.java
@@ -0,0 +1,37 @@
+package com.production.w.productionlinemonitor.Helper;
+
+/**
+ * Created by w on 4/26/2018.
+ */
+
+public class Coil {
+    public static final int systemRunning = 7;
+    public static final int systemStopped = 8;
+    public static final int systemError = 9;
+
+    public static final int station1Running = 3100;
+    public static final int station1Stopped = 3101;
+    public static final int station1Error = 3102;
+
+    public static final int station2Running = 3105;
+    public static final int station2Stopped = 3106;
+    public static final int station2Error = 3107;
+
+    public static final int station3Running = 3110;
+    public static final int station3Stopped = 3111;
+    public static final int station3Error = 3112;
+
+    public static final int station4Running = 3115;
+    public static final int station4Stopped = 3116;
+    public static final int station4Error = 3117;
+
+    public static final int station5Running = 3120;
+    public static final int station5Stopped = 3121;
+    public static final int station5Error = 3122;
+
+    public static final int outputFull = 400;
+
+    public static final int transmissionLineRunning = 3130;
+    public static final int transmissonLineStopped = 3136;
+
+}
---
+++ app/src/main/java/com/production/w/productionlinemonitor/Helper/Register.java
@@ -0,0 +1,25 @@
+package com.production.w.productionlinemonitor.Helper;
+
+/**
+ * Created by w on 4/26/2018.
+ */
+
+public class Register {
+    public static int systemTargetOutput = 4536;
+    public static int systemActualOutput = 4526;
+
+    public static int station1TargetOutput = 4510;
+    public static int station1ActualOutput = 4510;
+
+    public static int station2TargetOutput = 4512;
+    public static int station2ActualOutput = 4502;
+
+    public static int station3TargetOutput = 4514;
+    public static int station3ActualOutput = 4504;
+
+    public static int station4TargetOutput = 4516;
+    public static int station4ActualOutput = 4506;
+
+    public static int station5TargetOutput = 4518;
+    public static int station5ActualOutput = 4508;
+}
