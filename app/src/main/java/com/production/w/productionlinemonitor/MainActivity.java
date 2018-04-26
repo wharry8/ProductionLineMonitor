299ac20d9cb714eda2fe59aa9f7c61e3ea3bae17
TorikoC <breakthroughtgw@gmail.com>
2018-04-26 14:02:18 +0800

    update fetch api in MainActivity.

--- app/src/main/java/com/production/w/productionlinemonitor/MainActivity.java
+++ app/src/main/java/com/production/w/productionlinemonitor/MainActivity.java
@@ -10,6 +10,14 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.production.w.productionlinemonitor.Helper.Coil;
+import com.production.w.productionlinemonitor.Helper.Register;
+import com.zgkxzx.modbus4And.requset.ModbusParam;
+import com.zgkxzx.modbus4And.requset.ModbusReq;
+import com.zgkxzx.modbus4And.requset.OnRequestBack;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -24,16 +32,76 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv_up;
     private TextView tv_percent;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        initNavigationDrawer();
 
+        initNavigationDrawer();
+        initModbusInstance();
         bind();
-        updateView();
+//        updateView();
+        updateView2();
     }
+
+    public void initModbusInstance() {
+        ModbusReq.getInstance().setParam(new ModbusParam()
+                .setHost("192.168.0.105")
+                .setPort(502)
+                .setEncapsulated(false)
+                .setKeepAlive(true)
+                .setTimeout(2000)
+                .setRetries(0))
+                .init(new OnRequestBack<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        Log.d(TAG, "onSuccess " + s);
+                    }
+
+                    @Override
+                    public void onFailed(String msg) {
+                        Log.d(TAG, "onFailed " + msg);
+                    }
+                });
+    }
+    public void initNavigationDrawer() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+                        String selectedTitle = (String) menuItem.getTitle();
+                        Intent intent;
+
+                        if (selectedTitle == getString(R.string.main)) {
+
+                        } else if (selectedTitle == getString(R.string.productionLine)) {
+                            intent = new Intent(getApplicationContext(), ProductionLineActivity.class);
+                            startActivity(intent);
+
+                        } else if (selectedTitle == getString(R.string.workers)) {
+                            intent = new Intent(getApplicationContext(), WorkStationListActivity.class);
+                            startActivity(intent);
+
+                        } else {
+
+                        }
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        return true;
+                    }
+                });
+    }
+
     public void bind() {
         tv_status = findViewById(R.id.pl_tv_status);
         tv_speed = findViewById(R.id.pl_tv_speed);
@@ -44,8 +112,8 @@ public class MainActivity extends AppCompatActivity {
         tv_up = findViewById(R.id.tv_up);
         tv_percent = findViewById(R.id.ws_tv_percent);
     }
-
     public void updateView() {
+
         boolean running = isRunning();
         boolean stopped = isRunning();
         boolean error = isError();
@@ -109,6 +177,66 @@ public class MainActivity extends AppCompatActivity {
             tv_percent.setText(R.string.defaultPercent);
         }
     }
+    public void updateView2 () {
+        ModbusReq.getInstance().readCoil(new OnRequestBack<boolean[]>() {
+            @Override
+            public void onSuccess(boolean[] booleen) {
+                updateStatus(booleen);
+            }
+
+            @Override
+            public void onFailed(String msg) {
+                Log.e(TAG, "readCoil onFailed " + msg);
+            }
+        }, 1, 0, 10000);
+        ModbusReq.getInstance().readHoldingRegisters(new OnRequestBack<short[]>() {
+            @Override
+            public void onSuccess(short[] data) {
+                updateTarget(data);
+                updateCurrent(data);
+            }
+
+            @Override
+            public void onFailed(String msg) {
+                Log.e(TAG, "readHoldingRegisters onFailed " + msg);
+            }
+        }, 1, 0, 10000);
+    }
+
+    public void updateStatus (boolean[] booleen) {
+        boolean running = booleen[Coil.systemRunning];
+        boolean stopped = booleen[Coil.systemError];
+        boolean error = booleen[Coil.systemError];
+
+        if (running) {
+            tv_status.setText(R.string.normal);
+        } else if (stopped) {
+            tv_status.setText(R.string.stopped);
+        } else if (error) {
+            tv_status.setText(R.string.error);
+        } else {
+            tv_status.setText(R.string.unknown);
+        }
+    }
+    public void updateTarget (short[] data) {
+        short target = data[Register.systemTargetOutput];
+        tv_target.setText(Integer.toString(target));
+    }
+    public void updateCurrent (short[] data) {
+        short current = data[Register.systemActualOutput];
+        tv_current.setText(Integer.toString(current));
+    }
+    public void updateUp () {
+    }
+    public void updatePercent () {
+    }
+    public void updateSpeed () {
+    }
+    public void updateSystemRunningTime () {
+    }
+    public void updateMachineRunningTime () {
+    }
+
     public String getSpeedString(int speed) {
         return speed + ".0/min";
     }
@@ -117,43 +245,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public String getPercentString (double d) {
         return d + "%";
-    }
-
-
-    public void initNavigationDrawer() {
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        menuItem.setChecked(true);
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
-                        String selectedTitle = (String) menuItem.getTitle();
-                        Intent intent;
-
-                        if (selectedTitle == getString(R.string.main)) {
-
-                        } else if (selectedTitle == getString(R.string.productionLine)) {
-                            intent = new Intent(getApplicationContext(), ProductionLineActivity.class);
-                            startActivity(intent);
-
-                        } else if (selectedTitle == getString(R.string.workers)) {
-                            intent = new Intent(getApplicationContext(), WorkStationListActivity.class);
-                            startActivity(intent);
-
-                        } else {
-
-                        }
-
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
-                        return true;
-                    }
-                });
     }
     public boolean isRunning () {
         return false;
